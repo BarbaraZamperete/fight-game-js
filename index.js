d19b94b19bc9aa841fd9087f0c74eae95d8e9d58
barbarazamperete <bahzamp25@gmail.com>
2022-06-24 09:12:23 -0400

    FEAT: health bar

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ class Sprite {
       height: 50,
     };
     this.isAttacking;
+    this.health = 100
   }
 
   //método que insere o Sprite na cena
@@ -183,6 +184,8 @@ function animate() {
   ) {
     player.isAttacking = false;
     console.log("Atacando Player");
+    enemy.health -= 20
+    document.querySelector("#enemyHealth").style.width = enemy.health + '%'
   }
   if (
     rectangularCollision({ rectagle1: enemy, rectagle2: player }) &&
@@ -190,6 +193,8 @@ function animate() {
   ) {
     enemy.isAttacking = false;
     console.log("Atacando Enemy");
+    player.health -= 20
+    document.querySelector("#playerHealth").style.width = player.health + '%'
   }
 }
 animate();
